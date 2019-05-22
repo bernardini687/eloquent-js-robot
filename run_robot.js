d0eb9e724053ee1a31b826b1546dc9bd213c73e3
Oscar Bernardini <bernardini687@gmail.com>
2019-05-22 16:47:20 +0200

    Move robots into a new file

--- run_robot.js
+++ run_robot.js
@@ -1,6 +1,6 @@
 import roadGraph from './data/roads';
-import mailRoute from './data/mail_route';
 import VillageState from './village_state';
+import { randomPick, goalOrientedRobot } from './robots';
 
 const runRobot = (state, robot, memory) => {
   for (let turn = 0; ; turn += 1) {
@@ -13,43 +13,6 @@ const runRobot = (state, robot, memory) => {
     memory = action.memory;
     console.log(`Moved to ${action.direction}`);
   }
-};
-
-const randomPick = arr => arr[Math.floor(Math.random() * arr.length)];
-
-// pick a random direction from the nodes available at the current place
-// const randomRobot = state => ({ direction: randomPick(roadGraph[state.place]) });
-
-// const routeRobot = (state, memory) => {
-//   if (memory.length === 0) {
-//     memory = mailRoute;
-//   }
-//   return { direction: memory[0], memory: memory.slice(1) };
-// };
-
-const findRoute = (graph, from, to) => {
-  const work = [{ at: from, route: [] }];
-  for (let i = 0; i < work.length; i += 1) {
-    const { at, route } = work[i];
-    for (const place of graph[at]) {
-      if (place === to) return route.concat(place);
-      if (!work.some(w => w.at === place)) {
-        work.push({ at: place, route: route.concat(place) });
-      }
-    }
-  }
-};
-
-const goalOrientedRobot = ({ place, parcels }, route) => {
-  if (route.length === 0) {
-    const parcel = parcels[0];
-    if (parcel.place !== place) {
-      route = findRoute(roadGraph, place, parcel.place);
-    } else {
-      route = findRoute(roadGraph, place, parcel.address);
-    }
-  }
-  return { direction: route[0], memory: route.slice(1) };
 };
 
 VillageState.random = (parcelCount = 5) => {
