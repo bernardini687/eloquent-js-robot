0811845a524d69134a6b01d55042d7a3db5d5aa4
Oscar Bernardini <bernardini687@gmail.com>
2019-05-22 14:40:03 +0200

    Fix increment bug

--- run_robot.js
+++ run_robot.js
@@ -1,7 +1,8 @@
 import roadGraph from './data/roads';
+import VillageState from './village_state';
 
 const runRobot = (state, robot, memory) => {
-  for (let turn = 0; ; turn + 1) {
+  for (let turn = 0; ; turn += 1) {
     if (state.parcels.length === 0) {
       console.log(`Done in ${turn} turns`);
       break;
@@ -17,3 +18,18 @@ const randomPick = arr => arr[Math.floor(Math.random() * arr.length)];
 
 // pick a random direction from the nodes available at the current place
 const randomRobot = state => ({ direction: randomPick(roadGraph[state.place]) });
+
+VillageState.random = (parcelCount = 5) => {
+  const parcels = [];
+  for (let i = 0; i < parcelCount; i += 1) {
+    const address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place === address);
+    parcels.push({ place, address });
+  }
+  return new VillageState('Post Office', parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
